4aa037f3dad3aae21676f2c68584f926c8b47113
manojkumawatv <manojkumawatv@gmail.com>
2025-03-05 20:47:10 +0530

    Sorting alog: insertion & merge

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -1,9 +1,12 @@
 package dsa;
+import java.util.Arrays;
+import dsa.algorithms.Sorting;
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
+    public static void main(String[] args) {
+        int[] arr={5,8,4,22,78,5,48,72};
+        // Sorting.mergeSort(arr, 0, arr.length-1);
+        Sorting.mergeSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
 }
