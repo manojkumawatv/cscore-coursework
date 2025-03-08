b9fcea49955ad3165cc5e557b0979d1f59eff062
manojkumawatv <manojkumawatv@gmail.com>
2025-03-08 20:09:51 +0530

    LL:Queue introduced

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -1,6 +1,7 @@
 package dsa;
 import java.util.Arrays;
 import dsa.algorithms.*;
+import dsa.ds.Queue;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,10 @@ public class Main {
         // Sorting.quickSort(arr, 0, arr.length-1);
         Sorting_.countingSort(arr);
         System.out.println(Arrays.toString(arr));
+
+        Queue q1=new Queue();
+        q1.enqueue(7);
+        q1.enqueue(8);
+        q1.display();
     }
 }
