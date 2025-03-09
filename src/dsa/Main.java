79aaa990e46c995f45bd24b1d1c34d7bf440beab
manojkumawatv <manojkumawatv@gmail.com>
2025-03-09 18:37:31 +0530

    Linked List

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -2,19 +2,27 @@ package dsa;
 import java.util.Arrays;
 import dsa.algorithms.*;
 import dsa.ds.Queue;
+import dsa.ds.LL;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={5,8,4,22,78,5,48,72};
+        // int[] arr={5,8,4,22,78,5,48,72};
         // Sorting.mergeSort(arr, 0, arr.length-1);
-		System.out.println(Arrays.toString(arr));
+		// System.out.println(Arrays.toString(arr));
         // Sorting.quickSort(arr, 0, arr.length-1);
-        Sorting_.countingSort(arr);
-        System.out.println(Arrays.toString(arr));
 
-        Queue q1=new Queue();
-        q1.enqueue(7);
-        q1.enqueue(8);
-        q1.display();
+        // Sorting_.countingSort(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        // Queue q1=new Queue();
+        // q1.enqueue(7);
+        // q1.enqueue(8);
+        // q1.display();
+
+        LL list=new LL();
+        list.addAtHead(1);
+        list.addAtTail(5);
+        list.display();
+
     }
 }
---
+++ src/dsa/ds/LL.java
@@ -0,0 +1,97 @@
+package dsa.ds;
+
+class Node {
+    int val;
+    Node next;
+    Node(int val){
+        this.val=val;
+    }
+}
+public class LL {
+    private Node head;
+
+    public LL() {
+        head=null;
+    }
+    public void display(){
+        Node curr=head;
+        while(curr!=null){
+            System.out.print(curr.val+" ");
+            curr=curr.next;
+        }
+    }
+    public int get(int index) {
+        int i = 0;
+        Node curr = head;
+        while (curr != null) {
+            if (i == index) {
+                return curr.val;
+            }
+            i++;
+            curr = curr.next;
+        }
+        return -1;
+    }
+
+    public void addAtHead(int val) {
+        Node tmp = new Node(val);
+        if (head == null) {
+            head = tmp;
+        } else {
+            tmp.next = head;
+            head = tmp;
+        }
+    }
+
+    public void addAtTail(int val) {
+        Node tmp = new Node(val);
+        if(head==null) {
+            head=tmp;
+            return;
+        }
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        // now curr is tail
+        curr.next = tmp;
+    }
+
+    public void addAtIndex(int index, int val) {
+        Node curr = head;
+        int i = 0;
+        if(index<0) return;
+        
+        Node tmp = new Node(val);
+        if (index == 0) {
+            tmp.next=head;
+            head=tmp;
+            return;
+        }
+        while (curr != null && i < index - 1) {
+            i++;
+            curr = curr.next;
+        } // node at i-1 th index
+        if (curr == null) return; //IndexOutOfBounds
+        tmp.next = curr.next;
+        curr.next = tmp;
+    }
+
+    public void deleteAtIndex(int index) {
+        if(index<0 || head==null) return;
+        if(index==0){
+            head=head.next;
+            return;
+        }
+
+        Node curr=head;
+        int i=0;
+       
+        while(curr!=null && i<index-1){
+            i++;
+            curr=curr.next;
+        }
+        if(curr==null || curr.next==null) return; //IndexOutOfBounds as NPE 
+        curr.next=curr.next.next;
+    }
+}
