decdef687ea97773284acfd0971d28e634313ef9
manojkumawatv <manojkumawatv@gmail.com>
2025-03-11 14:27:42 +0530

    Queue:Array

--- src/dsa/ds/Queue.java
+++ src/dsa/ds/Queue.java
@@ -1,29 +1,55 @@
 package dsa.ds;
 
-//LinkedList Implementation
 public class Queue {
-    int val;
-    Queue next;
-    //LIFO
-    Queue head;
-    Queue tail;
-    public Queue(){
-    }
-    public void enqueue(int val){
-        Queue Node=new Queue();
-        Node.val=val;
-        if(head==null){
-            head=Node;
-        } else {
-            Node.next=head;
-            head=Node;
-        }
-    }
-    public void display(){
-        Queue curr=head;
-        while(curr!=null){
-            System.out.print(curr.val+", ");
-            curr=curr.next;
-        }
-    }
+	private int front, rear, maxSize; //maxSize is capacity
+	private String arr[];
+	Queue(int maxSize) {
+		this.front = -1; //queue created
+		this.rear = -1;
+		this.maxSize = maxSize;
+		this.arr = new String[this.maxSize];
+	}
+
+	public boolean isFull() {
+		if (rear == maxSize - 1) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean enqueue(String data) {
+		if (isFull()) {
+			return false;
+		} else {
+			arr[++rear] = data;
+			return true;
+		}
+	}
+
+	public void display() {
+		if (isEmpty())
+			System.out.println("Queue is empty!");
+		else {
+			System.out.println("Displaying queue elements");
+			for (int i = front; i <= rear; i++) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+
+	public boolean isEmpty() {
+		if (front > rear)
+			return true;
+		return false;
+	}
+
+	public String dequeue() {
+		if (isEmpty()) {
+			return "empty";
+		} else {
+			String data = arr[this.front];
+			arr[front++] = null; //since it's string arr
+			return data;
+		}
+	}
 }
---
+++ src/dsa/ds/list/Queue.java
@@ -0,0 +1,29 @@
+package dsa.ds;
+
+//LinkedList Implementation
+public class Queue {
+    int val;
+    Queue next;
+    //LIFO
+    Queue head;
+    Queue tail;
+    public Queue(){
+    }
+    public void enqueue(int val){
+        Queue Node=new Queue();
+        Node.val=val;
+        if(head==null){
+            head=Node;
+        } else {
+            Node.next=head;
+            head=Node;
+        }
+    }
+    public void display(){
+        Queue curr=head;
+        while(curr!=null){
+            System.out.print(curr.val+", ");
+            curr=curr.next;
+        }
+    }
+}
