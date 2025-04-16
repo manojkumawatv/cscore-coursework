d361039f5382d014c02c487d42364587fa0ef91b
manojkumawatv <manojkumawatv@gmail.com>
2025-04-16 21:47:42 +0530

    PowerSet

--- src/dsa/ds/Arrays.java
+++ src/dsa/ds/Arrays.java
@@ -29,6 +29,22 @@ public class Arrays extends ArraySearch {
         System.out.print("]");
         return "";
     }
+    public int[][] powerSet(int[] arr) {
+        int n = arr.length;
+        int powerSetSize = (int) Math.pow(2, n);
+        int[][] powerSet = new int[powerSetSize][];
+        for (int i = 0; i < powerSetSize; i++) {
+            int setSize = Integer.bitCount(i);
+            powerSet[i] = new int[setSize];
+            int index = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    powerSet[i][index++] = arr[j];
+                }
+            }
+        }
+        return powerSet;
+    }
     public static void main(String[] args) {
         int[] nums={1,2,3,4,5,6};
         Arrays arr= new Arrays(nums);
