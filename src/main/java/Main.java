bdd34b9b7969d00ef864e66c6b6328f073a6c893
manojkumawatv <manojkumawatv@gmail.com>
2025-07-28 00:28:14 +0530

    mvn minor

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+// import utils.NumberTheory;
+public class Main {
+    public static void main(String args[]){
+        // NumberTheory nt=new NumberTheory();
+        // nt.getPrimes(100);
+    }
+}
