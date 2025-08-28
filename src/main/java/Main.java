9d957f44641fd6ced1962eb8f5b60113bd23e15c
manojkumawatv <manojkumawatv@gmail.com>
2025-08-28 20:00:00 +0530

    Math: Matrix OOPs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 // import utils.NumberTheory;
+import utils.Mat;
 public class Main {
     public static void main(String args[]){
         // NumberTheory nt=new NumberTheory();
         // nt.getPrimes(100);
+        Mat m1=new Mat(4);
+        m1.display();
     }
 }
